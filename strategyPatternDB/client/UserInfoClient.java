d132cd90d280533c5b3739ba458660d1c5b8f18c
2winyear <lsy2246@naver.com>
2023-02-15 23:00:46 +0900

    New: strategyPatternDB

--- strategyPatternDB/client/UserInfoClient.java
+++ strategyPatternDB/client/UserInfoClient.java
@@ -4,13 +4,30 @@ import strategyPatternDB.userinfo.dao.*;
 import strategyPatternDB.userinfo.dao.mssql.*;
 import strategyPatternDB.userinfo.dao.mysql.*;
 import strategyPatternDB.userinfo.dao.oracle.*;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 import javax.management.*;
 
 public class UserInfoClient {
 	private static UserInfoDao userInfoDao;
 
 	private static void getDBProp() throws RuntimeErrorException {
-		String dbType = "";
+		String line = "";
+		try {
+			FileInputStream fis = new FileInputStream("strategyPatternDB/client/db.properties");
+			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+			line = br.readLine();
+			fis.close();
+		}
+		catch (Exception e) {
+			System.out.println(e);
+			System.exit(1);
+		}
+
+		String dbType = line.split("=")[1];
 
 		switch (dbType) {
 			case "MSSQL":
@@ -21,7 +38,7 @@ public class UserInfoClient {
 				break;
 			case "ORACLE":
 				UserInfoClient.userInfoDao = new UserInfoOracleDao();
-				break;	
+				break;
 			default:
 				throw new RuntimeErrorException(null);
 		}
