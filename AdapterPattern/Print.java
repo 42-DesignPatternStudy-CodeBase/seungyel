5b8cb687891278523fa2859e20743ae8ceb1ed41
2winyear <lsy2246@naver.com>
2023-03-05 21:10:31 +0900

    New: AdapterPattern

---
+++ AdapterPattern/Print.java
@@ -0,0 +1,6 @@
+package AdapterPattern;
+
+public interface Print {
+    public abstract void printWeak();
+    public abstract void printStrong();
+}
---
+++ AdapterPattern/PrintBanner.java
@@ -0,0 +1,14 @@
+package AdapterPattern;
+
+public class PrintBanner implements Print {
+    private Banner banner;
+    public PrintBanner(String string) {
+        this.banner = new Banner(string);
+    }
+    public void printWeak() {
+        banner.showWithParen();
+    }
+    public void printStrong() {
+        banner.showWithAster();
+    }
+}
